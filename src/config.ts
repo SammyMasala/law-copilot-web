cb43e153ba832faaba6e8284b4b5de1450ca0c30
ChickyMasala <61800906+SammyMasala@users.noreply.github.com>
2024-08-08 14:02:18 +0800

    ADDED trello href img (#10)

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,10 @@ export const CHAT_ENDPOINT = process.env.CHAT_ENDPOINT || "https://localhost:808
 export const SESSION_GET_ENDPOINT = process.env.SESSION_GET_ENDPOINT || "https://localhost:8080"
 export const SESSION_PUT_ENDPOINT = process.env.SESSION_PUT_ENDPOINT || "https://localhost:8080"
 
+// HREFS
+export const URL_TRELLO = "https://trello.com/b/kkimaj8A/law-copilot"
+export const URL_GITHUB = "https://github.com/SammyMasala/law-copilot-web"
+export const URL_ICONS8 = "https://icons8.com/icons"
+
 // AUTOSAVE
 export const AUTOSAVE_INTERVAL = 30
