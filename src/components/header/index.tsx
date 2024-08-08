f29646b70a712f80cb424534b09863eae6ecbaf5
ChickyMasala <samuel.goh96@gmail.com>
2024-08-08 14:00:55 +0800

    ADDED trello href img

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,8 +4,10 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
+import trello from "../../static/icons8-trello-25.png"
 import github from "../../static/icons8-github-25.png";
 import book from "../../static/icons8-book-25.png"
+import { URL_GITHUB, URL_ICONS8, URL_TRELLO } from "../../config";
 
 interface IHeaderProps {
     context: React.Context<any>
@@ -17,17 +19,23 @@ const Header: React.FC<IHeaderProps> = (props: IHeaderProps) => {
         <Container fluid>
             <Row className="d-flex">
                 <Col className="d-flex justify-content-start">
-                    <Image className="bg-light" src={book} roundedCircle/>
+                    <a href={sessionURL} className="me-1">
+                        <Image className="bg-light" src={book} roundedCircle/>
+                    </a>
                     <div> Law Copilot </div>
                 </Col>
                 <Col xs={2} className="d-flex justify-content-center">   
-                    <a href="https://github.com/SammyMasala/law-copilot-web">
-                        <Image className="bg-light" src={github} roundedCircle/>
-                    </a>                 
+                                 
                 </Col>
                 <Col className="d-flex justify-content-end">
+                    <a href={URL_TRELLO} className="me-1">
+                        <Image className="bg-light" src={trello} rounded/>
+                    </a>    
+                    <a href={URL_GITHUB} className="me-1">
+                        <Image className="bg-light" src={github} roundedCircle/>
+                    </a>    
                     <div>Icons by:</div>
-                    <a href="https://icons8.com/icons">Icon8</a>
+                    <a href={URL_ICONS8} className="ms-1 me-1">Icon8</a>
                 </Col>
             </Row>
             <Row className="d-flex">
